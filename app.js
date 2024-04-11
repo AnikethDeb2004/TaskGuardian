9aecfc59b31c418c9f44248fd4b3d98fe2daff60
Aniketh Deb <137791848+AnikethDeb2004@users.noreply.github.com>
2024-04-11 13:07:32 +0530

    chore: app.js

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const productRoutes = require('./routes/productRoutes');
-// const attributeRoutes = require('./routes/attributeRoutes');
-// const variantRoutes = require('./routes/variantRoutes');
 
-// const app = express();
-
-// app.use(bodyParser.json());
-
-// mongoose.connect('mongodb://localhost/product-variant-db');
-
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => console.log('Connected to MongoDB'));
-
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the product variant management system.');
-// }); 
-
-// app.use('/products', productRoutes);
-// app.use('/attributes', attributeRoutes);
-// app.use('/variants', variantRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-
-// app.js
-
-
-
-// app.js
-
-// app.js
 
 const express = require('express');
 const mongoose = require('mongoose');
