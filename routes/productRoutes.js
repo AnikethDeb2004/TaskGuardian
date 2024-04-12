b556416d19732765b5376b625acf3287e34d3ee9
Aniketh Deb <137791848+AnikethDeb2004@users.noreply.github.com>
2024-04-12 23:11:53 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
-// Add new product
+
 router.post('/', async (req, res) => {
   try {
     const product = new Product(req.body);
